9c9a1e73c9a3e0da8d9a272da2424a552657523f
mageshwarigowri <mageshwarigowri@gmail.com>
2026-06-26 22:23:19 +0530

    quick fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import { ClerkProvider } from '@clerk/clerk-react'
 
 // Import your Publishable Key
@@ -14,8 +14,8 @@ if (!PUBLISHABLE_KEY) {
 
 createRoot(document.getElementById('root')).render(
   <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-    <BrowserRouter basename="/QuickStayReact">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </ClerkProvider>,
 )
